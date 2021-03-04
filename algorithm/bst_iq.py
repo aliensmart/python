7a7f624afba90ac13822890f963f674b701576da
aliensmart <kaoua17@gmail.com>
2021-03-04 05:37:56 -0500

    changed the structure

--- algorithm/bst_iq.py
+++
@@ -1,2 +0,0 @@
-# Write an efficient algorithm thats able to compare two binary search trees. The method returns true if the trees are identical (same topology with same values in the nodes) otherwise it returns false.
-  
